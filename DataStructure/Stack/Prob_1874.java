14ad2434d19b70b4ba36b8e94d37f23b52256d86
sarahyoon <yoonsarah88@gmail.com>
2020-03-09 15:40:22 +0900

    Update Prob_1874.java
    edit comment

--- DataStructure/Stack/Prob_1874.java
+++ DataStructure/Stack/Prob_1874.java
@@ -2,6 +2,12 @@ package DataStructure;
 
 import java.io.*;
 import java.util.Stack;
+/*
+    problem_url: https://www.acmicpc.net/problem/1874
+    1) push 1 ~ n integer to stack
+    2) push in order until the given sequence
+    3) if given sequence is smaller than the top stack then return "NO" which is impossible to match with the sequence
+*/
 
 public class Prob_1874 {
 
@@ -10,7 +16,8 @@ public class Prob_1874 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
          int numOfSeq = Integer.parseInt(br.readLine());
          int[] arr = new int[numOfSeq];
-
+         
+        //insert sequence
         for(int i=0;i<numOfSeq;i++) {
              arr[i] = Integer.parseInt(br.readLine());
          }
@@ -18,10 +25,13 @@ public class Prob_1874 {
         Stack<Integer> stack = new Stack<Integer>();
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-         int order = 0;
+        
+        //# push to stack in order
+        int order = 0;
 
+        //arr: sequence
         for(int tmp : arr){
-
+            
             if(tmp > order){
                 while(tmp>order){
                     stack.push(++order);
